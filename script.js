6d2a1378f0accc45b5aa4c7106ef8b45981c3597
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2022-02-01 19:15:04 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -149,6 +149,10 @@ const updateUI = function (acc) {
 
   // Display summary
   calcDisplaySummary(acc);
+
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'lightgrey';
+  });
 };
 
 ///////////////////////////////////////
@@ -164,7 +168,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +186,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +210,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.ceil(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +227,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +255,86 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+/*
+console.log(23 === 23.0); //one data type for all numbers. stored in binary
+//base 10: 0 to 9
+//binary base 2: 0,1 This creates issues like below
+console.log(0.1 + 0.2); //returns 0.300000000000000004
+console.log(0.1 + 0.2 === 0.3); //returns false
+//converting str to number
+console.log(Number('23')); //one way of doing it
+console.log(+'23'); //an easier way of doing it
+
+// parsing
+console.log(Number.parseInt('30px', 10)); //will pull numbers out of a str if they are at the beginning. Accepts two arguments. The second is the radix which is the base number. We're using 10 but it could be 2 for binary.
+console.log(Number.parseInt('e23')); //returns NaN
+
+console.log(Number.parseFloat('2.5rem')); //reads decimal point from a string. if I used parseInt it would just return 2.
+
+console.log(Number.isNaN(20)); //returns true or false. This returns false
+console.log(Number.isNaN('20')); //so does this.
+console.log(Number.isNaN(+'20x')); //returns true
+console.log(Number.isNaN(23 / 0)); //returns false since this is infinity which doesn't maek sense. See Below.
+//isFinite is a better way to determine if it is a number.
+console.log(Number.isFinite(20)); //returns true
+console.log(Number.isFinite('20')); //returns false
+console.log(Number.isFinite(23 / 0)); //returns false
+*/
+/*
+// MATH AND ROUNDING
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2)); //It will do type coersion
+console.log(Math.max(5, 18, '23px', 11, 2)); // but not parsing, this will return NaN.
+
+console.log(Math.min(5, 18, 23, 11, 2));
+console.log(Math.PI * Number.parseFloat('10px') ** 2); //area of a circle
+
+console.log(Math.trunc(Math.random() * 6) + 1); //creates a random dice roll
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min; //creates a random int between max and min
+
+console.log(randomInt(10, 20));
+
+// Rounding integers
+console.log(Math.trunc(23.3)); //removes any decimal part. returns 23
+console.log(Math.round(23.9)); //rounds to the nearest integer. returns 24
+
+console.log(Math.ceil(23.3)); //rounds up
+console.log(Math.ceil(23.9)); //rounds up
+
+console.log(Math.floor(23.3)); //rounds down
+console.log(Math.floor(23.9)); //rounds down
+
+console.log(Math.trunc(-23.3)); //for negative numbers, trunc just cuts off the number at the decimal point.
+console.log(Math.floor(-23.3)); //for floor it rounds down to -24.
+
+//Rounding decimals
+console.log((2.7).toFixed(0)); //returns the string "3"
+console.log((2.7).toFixed(3)); //returns the string "2.700" it adds to the decimal side until it gets to 3
+console.log((2.345).toFixed(2)); //returns the string "2.35"
+console.log(+(2.345).toFixed(2)); //returns the num 2.35. makes sure + is outside the num brackets
+*/
+/*
+// THE REMAINDER OPERATOR
+
+console.log(5 % 2); //returns the remainder which is 1 in this case. 5 = 2 * 2 +1
+console.log(8 * 3); //returns 2 | 8 = 2*3 +2
+
+//checking even or odd
+console.log(6 % 2); //returns 0 which is even
+
+const isEven = num => num % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(18));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'lightgrey';
+  });
+});
+*/
